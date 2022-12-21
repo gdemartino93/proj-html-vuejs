3e0c29d9bba8081914aed38bd802783193dbe49f
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-12-21 16:29:48 +0100

    fix logos

--- src/components/JumboLogo.vue
+++ src/components/JumboLogo.vue
@@ -33,13 +33,13 @@ data(){
 <template>
 <section class="sect-logo">
     <div class="container">
-    <div class="logos d-flex justify-content-center align-items-center mx-2">
+    <div class="logos d-flex justify-content-around align-items-center mx-2">
         <img :src="logo.img" alt="" v-for="logo in logos" :key="logo" >
     </div>
 
 
 </div>
-<div class="prova">
+<div class="position-relative">
         <svg
             data-name="Layer 1"
             xmlns="http://www.w3.org/2000/svg"
@@ -53,7 +53,7 @@ data(){
             <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"></path>
         </svg>
 </div>
-    asdsadasdasdasd
+
 </section>
 
 
@@ -62,7 +62,7 @@ data(){
 <style scoped lang="scss">
 @use '../styles/partials/variables' as *;
 .logos{
-
+    min-height: 150px;
     img{
         width: 100px;
     }
