a432cba8efbcf70f1e6a5f4d35119b886be1bb71
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-12-21 14:18:32 +0100

    jumbotron imported

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
+import AppJumbotron from './components/AppJumbotron.vue';
 
 export default {
   name : "App",
-  components : { AppHeader }
+  components : { AppHeader, AppJumbotron }
 }
 </script>
 
 <template>
   <header>
     <AppHeader />
+    <AppJumbotron />
   </header>
 
 </template>
---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,33 @@
+<script>
+
+export default {
+data(){
+   return{
+    //Aggiungere nav links
+   }
+},
+}
+
+
+</script>
+
+<template>
+<div class="jumbotron">
+    <div class="container">
+  
+
+    </div>
+</div>
+
+
+
+</template>
+
+<style scoped lang="scss">
+@use '../styles/partials/variables' as *;
+.jumbotron{
+    background-color: #F8F9FC;
+    min-height: 50vh;
+}
+
+</style>
