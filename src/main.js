71517b13e20d684bbe6fe55ca46b84a11b10ce37
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-12-21 13:57:37 +0100

    installed fontawesome

--- src/main.js
+++ src/main.js
@@ -2,4 +2,25 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import "bootstrap/dist/css/bootstrap.css"
-createApp(App).mount('#app')
+
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faGithub, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
+import { faMagnifyingGlass, faSquare } from '@fortawesome/free-solid-svg-icons'
+
+
+
+/* add icons to the library */
+library.add(faUser,faStackOverflow,faGithub,faMagnifyingGlass,faSquare)
+
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
