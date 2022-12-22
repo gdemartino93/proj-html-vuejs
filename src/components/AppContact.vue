810d9f20a90b3827689278582e5d646b5a8ad309
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-12-22 17:41:23 +0100

    check empty string on input component contact

--- src/components/AppContact.vue
+++ src/components/AppContact.vue
@@ -3,9 +3,24 @@
 export default {
 data(){
    return{
-
+    isEmpty :false,
+    isError : Boolean,
+    userEmail : ""
    }
 },
+methods : {
+    checkEmpty(){
+        if (this.userEmail == ""){
+        this.isEmpty = true;
+        this.isError = true
+        }else{
+            this.isEmpty = false
+            this.isError = false
+            this.userEmail = ""
+        }
+    }
+
+}
 }
 
 
@@ -18,9 +33,17 @@ data(){
             <div class="col-6 offset-3 text-center">
                 <h2>Want to be among the first to find out?</h2>
                 <p class="my-4">Start working with <a href="">Landrick</a> that can provide everything you need to generate  awareness, drive traffic, connect. </p>
+                <!-- alert success  -->
+                <div class="alert alert-success" role="alert" v-if="isEmpty == false && isError == false">
+                Grazie per l'interessamento, la contatteremo presto.
+                </div>
+                <!-- errore in caso il value dell'input è vuoto -->
+                <div class="alert alert-danger" role="alert" v-if="isEmpty == true && isError == true">
+                Questo campo non può essere vuoto :D
+                </div>
                 <div class="input-group mb-3">
-                    <input class="form-control form-control-lg" type="text" placeholder="Email Address" aria-label=".form-control-lg example" id="email">
-                    <button class="btn btn-primary" type="button" id="getstarted">Get Started</button>
+                    <input class="form-control form-control-lg" type="text" placeholder="Email Address" aria-label=".form-control-lg example" id="email" v-model="userEmail">
+                    <button class="btn btn-primary" type="button" id="getstarted" @click="checkEmpty" formaction="submit">Get Started</button>
                 </div>
             </div>
             
